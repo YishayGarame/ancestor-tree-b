479579a35a0cd59e323251685647c8293f1e59d6
YishayGarame <yishatgr@gmail.com>
2020-04-19 14:22:20 +0300

    before tests

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -1,90 +1,318 @@
 #include "FamilyTree.hpp"
-//kust checking
-
 
 using namespace family;
 
 //Node
 
-   string findMyRelation(int level,string sex)
+string Node::findMyRelation(int level, string sex)
+{
+  if (level == 0)
+  {
+    return "Just Me";
+  }
+  else if (level == 1)
+  {
+    return sex;
+  }
+  else if (level == 2)
+  {
+    return "grand" + sex;
+  }
+  else
+  {
+    string relation = "grand" + sex;
+    for (int i = 3; i <= level; i++)
     {
-      if (level == 0)
-      {
-        return "Just Me";
-      }
-      else if(level == 1)
-      {
-          return sex;
-      }
-      else if (level == 2)
-      {
-        return "grand" + sex;
-      }
-      else
-      {
-        string relation = "grand" + sex;
-        for (int i = 3; i <= level; i++)
-        {
-            string temp = relation;
-            relation ="great-" + temp;
-        }
-       return relation;
-      }
+      string temp = relation;
+      relation = "great-" + temp;
     }
-
-
+    return relation;
+  }
+}
 
 //Tree
 
-        Tree::Tree(string name){};
+Tree &Tree::addFather(string child, string name)
+{
+  Node *found = findPerson(root, child);
+  if (found == NULL)
+  {
+    throw std::runtime_error(child + " does not exist");
+  }
+  else if (found->father != NULL)
+  {
+    throw std::runtime_error(child + " allready has a father");
+  }
+  else
+  {
+    Node *temp = new Node(name, found->level + 1);
+    found->father = temp;
+    temp->relation = temp->findMyRelation(temp->level, "father");
+  }
+  return *this;
+}
+Tree &Tree::addMother(string child, string name)
+{
+  Node *found = findPerson(root, child);
+  if (found == NULL)
+  {
+    throw std::runtime_error(child + " does not exist");
+  }
+  else if (found->mother != NULL)
+  {
+    throw std::runtime_error(child + " allready has a mother");
+  }
+  else
+  {
+    Node *temp = new Node(name, found->level + 1);
+    found->mother = temp;
+    temp->relation = temp->findMyRelation(temp->level, "mother");
+  }
+  return *this;
+}
+string Tree::relation(string name)
+{
+  Node *found = findPerson(root, name);
+  if (found == NULL)
+  {
+    throw std::runtime_error(name + " does not exist");
+  }
+  else
+  {
+    return found->relation;
+  }
+}
 
-       Tree& Tree::addFather(string child ,string name)
-        {
-            Node *a=findchild(root,name);
-            if(a==NULL)
-            {
-            //throw exception("fgfgf");
-            }
-            else if(a->father != NULL)
-            {
-            //throw exception("fgfgf");
+string Tree::find(string relation)
+{
+  Node *found = findReletive(root, relation);
+  if (found == NULL)
+  {
+    throw std::runtime_error(relation + " does not exist in this tree");
+  }
+  else
+  {
+    return found->name;
+  }
+}
 
-            }
-            else 
-            {
-              Node *b=new Node(name,a->level+1);
-              a->father=b;
-              b->relation = findMyRelation(b->level,"father");
-            }
-        }
-      Tree& Tree::addMother(string child,string name)
-        {
-            return *this;  
-        }
+void Tree::display()
+{
+  printInorder(root);
+};
+void Tree::printInorder(Node *node)
+{
+  if (node == NULL)
+    return;
 
-      string Tree::relation(string)
-        {
-            string s="relation";
-            return s;
-        }
+  /* first recur on left child */
+  printInorder(node->father);
 
-        string Tree::find(string)
-        {
-             string s="find";
-            return s;
-        }
+  /* then print the data of node */
+  cout << node->name << " ";
 
-        void Tree::display()
-        {
+  /* now recur on right child */
+  printInorder(node->mother);
+}
 
-        };
+void Tree::remove(string name)
+{
+  if (root->name == name)
+  {
+    throw std::runtime_error(name + "is the Root of the tree so you cant remove it");
+  }
+  else
+  {
+    Node *found = findChild(root, name);
+    if (found == NULL)
+    {
+      throw std::runtime_error(name + " doesnt belong to the tree");
+    }
+    if (found->father != NULL && found->father->name == name) //delete father sub-tree
+    {
+      deleteParent(found->father);
+      found->father = NULL;
+    }
+    else if (found->mother != NULL && found->mother->name == name) //delete mother sub-tree
+    {
+      deleteParent(found->mother);
+      found->mother = NULL;
+    }
+  }
+};
+void Tree::deleteParent(Node *remove)
+{
+  if (remove->father == NULL && remove->mother == NULL)
+  {
+    delete remove;
+  }
+  else if (remove->father != NULL && remove->mother == NULL)
+  {
+    deleteParent(remove->father);
+    remove->father = NULL;
+    delete remove->father;
+  }
+  else if (remove->mother != NULL && remove->father == NULL)
+  {
+    deleteParent(remove->mother);
+    remove->mother = NULL;
+    delete remove->mother;
+  }
+  else
+  {
+    deleteParent(remove->father);
+    remove->father = NULL;
+    deleteParent(remove->mother);
+    remove->mother = NULL;
+    delete remove->father;
+    delete remove->mother;
+  }
+};
 
-        void Tree::remove(string name)
-        {
+Node *Tree::findPerson(Node *node, string name)
+{
+  if (node != NULL)
+  {
+    if (node->name == name)
+    {
+      return node;
+    }
+    else
+    {
+      Node *foundNode = findPerson(node->father, name);
+      if (foundNode == NULL)
+      {
+        foundNode = findPerson(node->mother, name);
+      }
+      return foundNode;
+    }
+  }
+  else
+  {
+    return NULL;
+  }
+}
 
-        };
+Node *Tree::findReletive(Node *node, string relation)
 
-        Node* findchild(Node *root,string name)
-        {
+{
+  if (node != NULL)
+  {
+    if (node->relation == relation)
+    {
+      return node;
+    }
+    else
+    {
+      Node *foundNode = findReletive(node->father, relation);
+      if (foundNode == NULL)
+      {
+        foundNode = findReletive(node->mother, relation);
+      }
+      return foundNode;
+    }
+  }
+  else
+  {
+    return NULL;
+  }
+}
 
-        }
+Node *Tree::findChild(Node *node, string name)
+{
+  if (node != NULL)
+  {
+    if ((node->father != NULL && node->father->name == name) || (node->mother != NULL && node->mother->name == name))
+    {
+      return node;
+    }
+    else
+    {
+      Node *foundNode = findChild(node->father, name);
+      if (foundNode == NULL)
+      {
+        foundNode = findChild(node->mother, name);
+      }
+      return foundNode;
+    }
+  }
+  else
+  {
+    return NULL;
+  }
+}
+
+//         #include <iostream>
+// #include <string>
+// using namespace std;
+
+// // Data structure to store a Binary Tree node
+// struct Node
+// {
+//     int data;
+//     Node *left, *right;
+
+//     Node(int data)
+//     {
+//         this->data = data;
+//         this->left = this->right = nullptr;
+//     }
+// };
+
+// struct Trunk
+// {
+//     Trunk *prev;
+//     string str;
+
+//     Trunk(Trunk *prev, string str)
+//     {
+//         this->prev = prev;
+//         this->str = str;
+//     }
+// };
+
+// // Helper function to print branches of the binary tree
+// void showTrunks(Trunk *p)
+// {
+//     if (p == nullptr)
+//         return;
+
+//     showTrunks(p->prev);
+
+//     cout << p->str;
+// }
+
+// // Recursive function to print binary tree
+// // It uses inorder traversal
+// void printTree(Node *root, Trunk *prev, bool isLeft)
+// {
+//     if (root == nullptr)
+//         return;
+
+//     string prev_str = "    ";
+//     Trunk *trunk = new Trunk(prev, prev_str);
+
+//     printTree(root->left, trunk, true);
+
+//     if (!prev)
+//         trunk->str = "---";
+//     else if (isLeft)
+//     {
+//         trunk->str = ".---";
+//         prev_str = "   |";
+//     }
+//     else
+//     {
+//         trunk->str = "`---";
+//         prev->str = prev_str;
+//     }
+
+//     showTrunks(trunk);
+//     cout << root->data << endl;
+
+//     if (prev)
+//         prev->str = prev_str;
+//     trunk->str = "   |";
+
+//     printTree(root->right, trunk, false);
+// }
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -1,45 +1,42 @@
-#include <iostream> 
-#include <string> 
-using namespace std; 
+#include <iostream>
+#include <string>
+using namespace std;
 
 namespace family
 {
-    class Node
-    {
-    public:
-    string name; 
+class Node
+{
+public:
+    string name;
     string relation;
     int level;
-    Node *mother, *father; 
+    Node *mother, *father;
 
-   
+    //consructor and functions
 
-    Node(string name,int level):
-    name(name), relation("Just me") , level(level), mother(NULL),father(NULL){};
+    Node(string name, int level) : name(name), relation("Just me"), level(level), mother(NULL), father(NULL){};
 
-    string findMyRelation(int level,string sex);
+    string findMyRelation(int level, string sex);
+};
 
+class Tree
+{
+public:
+    Node *root;
 
-    };
- 
-    class Tree 
-    {
-    private:
-    Node * root;
+    Tree(string name) : root(new Node(name, 0)){};
 
-    public:
+    Tree &addFather(string child, string father);
+    Tree &addMother(string child, string mother);
+    string relation(string relation);
+    string find(string name);
+    void display();
+    void remove(string name);
+    void deleteParent(Node *remove);
+    Node *findReletive(Node *root, string relation);
+    Node *findPerson(Node *root, string name);
+    Node *findChild(Node *node, string name);
+    void printInorder(Node *node);
+};
 
-    Tree(string name);
-
-        Tree& addFather(string child,string father);
-        Tree& addMother(string child,string mother);
-        string relation(string relation);
-        string find(string name);
-        void display();
-        void remove(string name);
-        Node* findchild(Node *root,string name);
-
-    };
-
-
-}
+} // namespace family
